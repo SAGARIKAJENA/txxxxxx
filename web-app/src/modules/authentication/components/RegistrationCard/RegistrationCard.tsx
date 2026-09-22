211a37326e5a6b46a0b9b0adc9c20df8233b9c1c
SAGARIKAJENA <jenasagarika211@gmail.com>
2026-09-22 09:43:30 +0530

    style: rename to 'Back' and style as grey capsule button

--- web-app/src/modules/authentication/components/RegistrationCard/RegistrationCard.tsx
+++ web-app/src/modules/authentication/components/RegistrationCard/RegistrationCard.tsx
@@ -76,7 +76,7 @@ export const RegistrationCard: React.FC = () => {
               <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="reg-card__back-icon">
                 <path d="M19 12H5M12 19l-7-7 7-7" />
               </svg>
-              <span>Back to Step 1</span>
+              <span>Back</span>
             </button>
           </div>
 
